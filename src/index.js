a00016307f30dff9d9d7f9b4a22050dfb8226715
ulianovikova <yasenevsk.18@gmail.com>
2023-05-13 15:20:03 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
 import {StoreProvider} from "./stateManegement/store";
 import SocketService from "./socketService";
 import Router from "./router"
